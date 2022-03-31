0d5c1e51af5f5fdaf8a965ca626e86df3772a63f
dante.panella.contractor.gdvetools.com <dante.panella.contractor@gdevtools.com>
2022-03-31 20:55:27 +0200

    fix magia

--- src/main/java/com/bbva/cards/dto/Cards.java
+++ src/main/java/com/bbva/cards/dto/Cards.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Cards {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	@Column(name = "number")
 	private int numberCard;
